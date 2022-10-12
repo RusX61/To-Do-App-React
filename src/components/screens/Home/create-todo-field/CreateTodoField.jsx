8d2b9c5bbb6cd9855b814f465534810ebc57b251
Ruslan Merzlyakov <rusx.lan@gmail.com>
2022-10-12 13:09:51 +0300

    fix mt

--- src/components/screens/Home/create-todo-field/CreateTodoField.jsx
+++ src/components/screens/Home/create-todo-field/CreateTodoField.jsx
@@ -16,7 +16,7 @@ const CreateTodoField = ({ setTodos }) => {
 	}
 
 	return (
-		<div className='flex items-center justify-between mb-4 rounded-2xl border-zinc-800 border-2 px-5 py-3 w-full mt-20'>
+		<div className='flex items-center justify-between mb-1 rounded-2xl border-zinc-800 border-2 px-5 py-3 w-full mt-10'>
 			<input
 				type='text'
 				onChange={e => setTitle(e.target.value)}
